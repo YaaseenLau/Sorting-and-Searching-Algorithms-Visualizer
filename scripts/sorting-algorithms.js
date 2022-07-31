2985bd967109cdecfae5fd5d50f6d85969b887b1
yaalau021 <yaalau021@student.wethinkcode.co.za>
2022-07-31 22:56:18 +0200

    Added insertion sort

--- scripts/sorting-algorithms.js
+++ scripts/sorting-algorithms.js
@@ -13,11 +13,38 @@ function bubbleSort(list) {
     return list; 
 }
 
+function insertionSort(list){  
+    let i, key, j;  
+    for (i = 1; i < list.length; i++){  
+        key = list[i];  
+        j = i - 1;  
+    
+        while (j >= 0 && list[j] > key){  
+            list[j + 1] = list[j];  
+            j = j - 1;  
+        }  
+        list[j + 1] = key;  
+    }  
+    return list;
+}  
+
+function sortSelector(sortName, list){
+    switch(sortName) {
+        case "bubbleSort":
+            document.write("Bubble Sort:<br>");
+            return bubbleSort(list);
+        case "insertionSort":
+            document.write("Insertion Sort:<br>");
+            return insertionSort(list);
+        default:
+          // code block
+      }
+}
+
 function main(){
     const unsorted = [6,5,3,1,8,7,2,4];
-    document.write("Bubble Sort:<br>");
     document.write("<br>Unsorted list = "+unsorted+"<br>");
-    document.write("Sorted list = "+bubbleSort(unsorted));
+    document.write("Sorted list = "+sortSelector("insertionSort",unsorted));
 }
 
 main();
